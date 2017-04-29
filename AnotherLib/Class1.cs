949ecb3a6ed1b480456259075413dfc5b8edff6b
Denis Zavershinskiy <zaverden@gmail.com>
2017-04-29 13:58:07 +0700

    sample project

---
+++ AnotherLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherLib
+{
+    public class Class1
+    {
+    }
+}
